1098b35ec21c79a7688b3f5652c0b7d44a4b8aa5
Artem Wolkov <wolkov.art.leo@yandex.ru>
2020-02-24 20:06:38 +0300

    all task

---
+++ src/index.js
@@ -0,0 +1,61 @@
+/* ДЗ 6 - Асинхронность и работа с сетью */
+
+/*
+ Задание 1:
+
+ Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
+
+ Пример:
+   delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
+ */
+function delayPromise(seconds) {
+    seconds = 1000;
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, seconds);
+    });
+}
+
+/*
+ Задание 2:
+
+ 2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов можно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+
+ 2.2: Элементы полученного массива должны быть отсортированы по имени города
+
+ Пример:
+   loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
+ */
+function loadAndSortTowns() {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest(); // создание объекта (ajax) для http запроса
+        const url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+
+        xhr.open('GET', url); // инициализируем его указывая метод и куда будет отправлен запрос
+        xhr.responseType = 'json'; // указываем тип получаемого ответа (формат)
+        xhr.send(); // посылаем запрос
+
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 400) {
+                reject();
+            } else {
+                resolve(xhr.response.sort((a,b) => {
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    return 0;
+                }));
+            }
+        });
+    });
+}
+
+export { delayPromise, loadAndSortTowns };
